32391c61692ab260806da042d2b94ffa16d0ee61
ZvonimirZlo <lovrez2@gmail.com>
2025-06-19 17:37:19 +0200

    Add variables

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-
+//Variables
 const descriptionButton = document.getElementById('descBtn');
 const description = document.getElementById("description");
+const encryptTextBox = document.getElementById('textEnc');
+const encryptOut = document.getElementById('textEncOut');
+const decryptTextBox = document.getElementById('textDec');
+const decryptOut = document.getElementById('textDecOut');
+const encryptButton = document.getElementById('encBtn');
+const slider = document.getElementById('switch');
+const decryptButton = document.getElementById('decryptBtn');
 
 //Shows/hides text box with instructions
 const showHideTextBox = () => {
@@ -13,6 +20,7 @@ const showHideTextBox = () => {
 
 descriptionButton.onclick = showHideTextBox;
 
+//Hides description text box on page reload
 window.onload = () => {
    description.style.display = "none";
 }
