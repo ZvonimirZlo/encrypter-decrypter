f211833b6fb952f6549166420af14e74b92c092b
ZvonimirZlo <lovrez2@gmail.com>
2025-06-19 16:10:46 +0200

    Add 'How it works' button and textarea

---
+++ script.js
@@ -0,0 +1,8 @@
+    function showHideTextBox(){
+      var x = document.getElementById("city");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
